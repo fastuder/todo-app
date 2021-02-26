4800e826d4fbd864deb5482f052475e4289a3add
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 15:37:26 +0100

    edit App.vue: app component structure

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+  <v-main>
+    <Header></Header>
+    <v-container fluid>
+      <TodoList></TodoList>
+      <v-dialog
+        v-model="showNewTodoDialog"
+      >
+        <NewTodo></NewTodo>
+      </v-dialog>
+    </v-container>
+    <v-fab-transition>
+      <v-btn
+      fab
+      absolute
+      bottom
+      right
+      @click="toggleNewTodoDialog"
+      class="add-todo-btn"
+      >
+      <v-icon color="primary">
+          mdi-plus
+      </v-icon>
+    </v-btn>
+    </v-fab-transition>
+  </v-main>
+</v-app>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+
+// Components
+import TodoList from "@/components/todo-list/todo-list.component.vue";
+import Header from "@/components/header/header.component.vue";
+import NewTodo from "@/components/new-todo/new-todo.component.vue";
+import { mapGetters, mapActions } from "vuex";
 
 @Component({
+  computed: mapGetters([
+    "showNewTodoDialog"
+  ]),
+  methods: mapActions([
+    "toggleNewTodoDialog"
+  ]),
   components: {
-    HelloWorld,
+    TodoList,
+    NewTodo,
+    Header
   },
 })
-export default class App extends Vue {}
-</script>
+export default class App extends Vue {
+  showNewTodoDialog!: boolean;
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  toggleNewTodoDialog!: () => void;
 }
-</style>
+</script>
