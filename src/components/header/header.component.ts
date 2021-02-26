beadfdc76ae3e87760d86b260fa93c277e4cce71
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 15:37:58 +0100

    add header/: header component

---
+++ src/components/header/header.component.ts
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import { mapActions } from 'vuex';
+
+@Component({})
+export default class Header extends Vue {}
+
---
+++ src/components/header/header.component.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app-bar app>
+        <v-app-bar-title>
+            <h2>Todo List</h2>
+        </v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<script src="./header.component.ts" lang="ts"></script>
+<style src="./header.component.scss" scoped lang="scss"></style>
