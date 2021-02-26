54a77341f2143f57d0ed0ac70694c9a5577c0083
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 15:37:01 +0100

    .

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+
+import "@/assets/scss/_global.scss";
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  store,
+  render: h => h(App)
 }).$mount('#app')
